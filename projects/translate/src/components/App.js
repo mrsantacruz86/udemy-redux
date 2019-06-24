829c4102d7dc1286aed18addd843ef7fa8cc2604
Nelson Diaz <abnerdc86@gmail.com>
2019-06-24 18:45:02 -0400

    setup main conponents

---
+++ projects/translate/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import UserCreate from "./UserCreate";
+
+class App extends Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select language
+          <i className="flag us" onClick={() => this.onLanguageChange("english")} />
+          <i className="flag es" onClick={() => this.onLanguageChange("spanish")} />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
