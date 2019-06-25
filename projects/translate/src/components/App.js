2fc52d381a019575ca1827fe722ae010c784ddb2
Nelson Diaz <abnerdc86@gmail.com>
2019-06-25 02:18:40 -0400

    adding context #1

--- projects/translate/src/components/App.js
+++ projects/translate/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends Component {
   state = { language: "english" };
@@ -16,7 +17,9 @@ class App extends Component {
           <i className="flag us" onClick={() => this.onLanguageChange("english")} />
           <i className="flag es" onClick={() => this.onLanguageChange("spanish")} />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
--- projects/translate/src/components/Button.js
+++ projects/translate/src/components/Button.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Enviar";
+    return <button className="ui button primary">{text}</button>;
   }
 }
+
+// This does the same as the statement in line 5
+// Button.contextType = LanguageContext;
 
 export default Button;
