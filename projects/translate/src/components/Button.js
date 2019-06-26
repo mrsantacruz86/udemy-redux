c939c1263e90e2071c71c07d01fad05f6344cf3e
Nelson Diaz <abnerdc86@gmail.com>
2019-06-26 16:44:20 -0400

    finishing the context example

--- projects/translate/src/components/Button.js
+++ projects/translate/src/components/Button.js
@@ -3,18 +3,26 @@ import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 
 class Button extends Component {
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Enviar";
+  }
+
   renderButton = color => {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {value => (value === "english" ? "Submit" : "Enviar")}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
   };
 
   render() {
-    return <ColorContext.Consumer>{color => this.renderButton(color)}</ColorContext.Consumer>;
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
   }
 }
 
--- projects/translate/src/components/LanguageSelector.js
+++ projects/translate/src/components/LanguageSelector.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class LanguageSelector extends Component {
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return (
       <div>
         Select language
-        <i className="flag us" onClick={() => this.props.onLanguageChange("english")} />
-        <i className="flag es" onClick={() => this.props.onLanguageChange("spanish")} />
+        <i
+          className="flag us"
+          onClick={() => this.context.onLanguageChange("english")}
+        />
+        <i
+          className="flag es"
+          onClick={() => this.context.onLanguageChange("spanish")}
+        />
       </div>
     );
   }
